34276b1c8dc959e66233337ceddbc36effcf8ecc
jacketattack <trvrmay@yahoo.com>
2014-08-01 12:52:59 -0700

    shortened lines for PEP8 style

--- dopy/domain_record.py
+++ dopy/domain_record.py
@@ -16,10 +16,13 @@ class DomainRecord(object):
     @classmethod
     def create_new(cls, domain_name, data, authentication_token):
 
-        create_domain_record_response = utils.post_request("domains/{0}/records".format(domain_name), data, authentication_token)
+        create_domain_record_response = utils.post_request("domains/{0}/records"
+                .format(domain_name), data, authentication_token)
+
         if not create_domain_record_response.ok:
 
-            raise Exception("Failed to create domain record! See error message: {0}".format(create_domain_record_response.json()["message"]))
+            raise Exception("Failed to create domain record! See error message: 
+                    {0}".format(create_domain_record_response.json()["message"]))
 
         domain_record = create_domain_record_response.json()["domain_record"]
         domain_record["domain_name"]  = domain_name
@@ -28,10 +31,13 @@ class DomainRecord(object):
     @classmethod
     def from_existing(cls, domain_name, record_id, authentication_token):
 
-        get_domain_record_response = utils.get_request("domains/{0}/records/{1}".format(domain_name, record_id), authentication_token)
+        get_domain_record_response = utils.get_request("domains/{0}/records/{1}"
+                .format(domain_name, record_id), authentication_token)
+
         if not get_domain_record_response.ok:
 
-            raise Exception("Failed to get domain record! See error message: {0}".format(get_domain_record_response.json()["message"]))
+            raise Exception("Failed to get domain record! See error message: {0}"
+                    .format(get_domain_record_response.json()["message"]))
 
         domain_record = get_domain_record_response.json()["domain_record"]
         domain_record["domain_name"]  = domain_name
@@ -39,10 +45,13 @@ class DomainRecord(object):
 
     def delete(self):
 
-        delete_domain_record_response = utils.delete_request("domains/{0}/records/{1}".format(self.domain_name, self.id), self.authentication_token)
+        delete_domain_record_response = utils.delete_request("domains/{0}/records/{1}"
+                .format(self.domain_name, self.id), self.authentication_token)
+
         if not delete_domain_record_response.ok:
 
-            raise Exception("Failed to delete domain record! See error message: {0}".format(delete_domain_record_response.json()["message"]))
+            raise Exception("Failed to delete domain record! See error message: {0}"
+                    .format(delete_domain_record_response.json()["message"]))
 
     def change_name(self, new_name):
 
@@ -50,6 +59,7 @@ class DomainRecord(object):
         change_name_response = utils.put_request("domains/{0}/records/{1}".format(self.domain_name, self.id))
         if not change_name_response.ok:
 
-            raise Exception("Failed to change name of domain record! See error message: {0}".format(change_name_response.json()["message"]))
+            raise Exception("Failed to change name of domain record! See error message: {0}"
+                    .format(change_name_response.json()["message"]))
 
         self.name = new_name
